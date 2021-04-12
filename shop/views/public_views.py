e632980e79086587ec167b5da7f069ef51824f2b
ian <is@ktconsultants.co.uk>
2021-04-12 15:32:11 +0100

    Add recaptcha

--- shop/views/public_views.py
+++ shop/views/public_views.py
@@ -253,6 +253,7 @@ class EnquiryView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["captcha_site"] = settings.GOOGLE_RECAPTCHA_SITE_KEY
         return add_page_context(context, "contact")
 
     def post(self, request, *args, **kwargs):
