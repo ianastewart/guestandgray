d416ff0e9d3a3eef6cc5d86b6f312c27844ba611
ian <is@ktconsultants.co.uk>
2021-08-27 19:37:58 +0100

    Wagtail seo working

--- shop/migrations/0040_hostpage.py
+++ shop/migrations/0040_hostpage.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("coderedcms", "0023_auto_20210819_0828"),
         ("shop", "0039_item_updated"),
     ]
 
