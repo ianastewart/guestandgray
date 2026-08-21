c0862b3b4a47557279262e87691de721682b855a
ianastewart <is@iskt.co.uk>
2026-08-21 19:01:19 +0100

    Adding log rotate

--- deployment/first_setup.sh
+++ deployment/first_setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Usage: first_setup.sh <app-name>
+APP_NAME="${1:?Usage: first_setup.sh <app-name>}"
+
 adduser django
 usermod -aG sudo django
 mkdir /home/django/.ssh
@@ -6,3 +9,9 @@ cp ~/.ssh/authorized_keys /home/django/.ssh
 chown -R django:django /home/django
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service ssh restart
+
+# nginx access/error logs live under /home/django/$APP_NAME/logs, outside the
+# paths covered by the distro's own /etc/logrotate.d/nginx, so install a
+# dedicated rotation rule for them.
+sed "s/XXapp/$APP_NAME/g" "$(dirname "$0")/logrotate_nginx" > "/etc/logrotate.d/$APP_NAME-nginx"
+chmod 644 "/etc/logrotate.d/$APP_NAME-nginx"
